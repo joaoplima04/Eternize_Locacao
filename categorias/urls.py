0def4ec797b40373efc018bda1b31cff1687037e
João Lucas Pereira <joao.plima@sempreceub.com>
2024-05-06 23:25:16 -0300

    Initial commit

---
+++ categorias/urls.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.index),
+    path('guardanapos/', views.guardanapos),
+    path('souplats/', views.souplats),
+    path('jogos_americanos/', views.jogos_americanos),
+    path('pratos/', views.pratos),
+    path('pratos_sobremesa/', views.pratos_sobremesa),
+    path('porta_guardanapos/', views.porta_guardanapos),
+    path('tacas/', views.tacas),
+    path('talheres/', views.talheres),
+    path('trilhos_de_mesa/', views.trilhos_de_mesa),
+    path('cha_e_cafe_da_tarde/', views.cha_e_cafe_da_tarde),
+]
--- categorias/views.py
+++ categorias/views.py
@@ -2,6 +2,35 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('')
+    return render(request, 'categorias/index.html')
 
-# Create your views here.
+def guardanapos(request):
+    return render(request, 'categorias/guardanapos.html')
+
+def souplats(request):
+    return render(request, 'categorias/souplats.html')
+
+def jogos_americanos(request):
+    return render(request, 'categorias/jogos_americanos.html')
+
+def pratos(request):
+    return render(request, 'categorias/pratos.html')
+
+def pratos_sobremesa(request):
+    return render(request, 'categorias/pratos_sobremesa.html')
+
+def porta_guardanapos(request):
+    return render(request, 'categorias/porta_guardanapos.html')
+
+def tacas(request):
+    return render(request, 'categorias/tacas.html')
+
+def talheres(request):
+    return render(request, 'categorias/talheres.html')
+
+def trilhos_de_mesa(request):
+    return render(request, 'categorias/trilhos_de_mesa.html')
+
+def cha_e_cafe_da_tarde(request):
+    return render(request, 'categorias/cha_e_cafe_da_tarde.html')
+
