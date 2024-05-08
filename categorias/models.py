15feb9a44cd20e6b32223b39c7adfa74a264b90f
joao.plima <joao.plima@sempreceub.com>
2024-05-08 10:40:34 -0300

    Commit de alterações no model e admin

--- categorias/models.py
+++ categorias/models.py
@@ -4,7 +4,7 @@ from django.utils import timezone
 class Produto(models.Model):
     nome = models.CharField(max_length=100)
     categoria = models.CharField(max_length=100)
-    preco = models.DecimalField(max_digits=5)
+    preco = models.DecimalField(decimal_places=2, max_digits=10)
     quantidade_estoque = models.IntegerField
     imagem = models.CharField(max_length=100)
     cor = models.CharField(max_length=100)
@@ -13,24 +13,24 @@ def __str__(self):
     return self.nome
 
 class Cliente(models.Model):
-    cpf = models.IntegerField(max_length=11).primary_key
+    cpf = models.CharField(max_length=11, primary_key=True, default='11111111111') 
     nome = models.CharField(max_length=100)
-    email = models.EmailField
-    telefone = models.IntegerField(max_length=11)
-    data_nascimento = models.DateField
+    email = models.EmailField()  
+    telefone = models.CharField(max_length=20)  
+    data_nascimento = models.DateField(default=timezone.now)  
+
 
     def __str__(self):
         return self.nome
 
 class Aluguel(models.Model):
     cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
-    produto = models.ForeignKey(Produto, on_delete=models.CASCADE)
     data_aluguel = models.DateField(default=timezone.now)
     data_devolucao = models.DateField()
-    preco_total = models.DecimalField(max_digits=5)
+    preco_total = models.DecimalField(decimal_places=2, max_digits=10)
 
 class ItemAluguel(models.Model):
     aluguel = models.ForeignKey(Aluguel, on_delete=models.CASCADE)
     produto = models.ForeignKey(Produto, on_delete=models.CASCADE)
     quantidade = models.PositiveIntegerField(default=1)
-    preco_item = models.DecimalField(max_digits=5)
+    preco_item = models.DecimalField(decimal_places=2, max_digits=10)
